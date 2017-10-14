86dd3510896ec7b4f9270ce3aa41fd19f9b18520
BamBo <raksameth@gmail.com>
2017-10-14 15:41:15 +0700

    .

--- Egg-Timer/app/src/main/java/com/raksa/eggtimer/MainActivity.java
+++ Egg-Timer/app/src/main/java/com/raksa/eggtimer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.raksa.eggtimer;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        };
+
+
+
+
     }
 }
